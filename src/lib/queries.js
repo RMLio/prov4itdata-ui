9c1f2f4355abb90b4fe4c26b95318c4cf6ad6909
Gertjan De Mulder <gertjan.demulder@ugent.be>
2021-03-08 17:16:23 +0100

    update queries

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,6 +1,27 @@
 export const queryRecords = {
-        "construct_image_triples": {
-        "description" : "Construct image triples",
+    "construct_image_triples": {
+        "description": "Construct image triples",
+        "query":
+            `
+             PREFIX schema: <http://schema.org/> 
+            PREFIX dcat: <http://www.w3.org/ns/dcat#> 
+            
+            CONSTRUCT { ?s ?p ?o .   } WHERE { 
+                {
+                    ?s ?p ?o .
+                    FILTER EXISTS { ?s a schema:ImageObject}
+                }
+                UNION
+                {
+                    ?s ?p ?o .
+                    FILTER EXISTS { ?s a dcat:Distribution}
+                }
+            
+            }
+            `
+    },
+    "construct_image_collections": {
+        "description": "Construct image collections",
         "query":
             `
             PREFIX schema: <http://schema.org/> 
@@ -8,18 +29,16 @@ export const queryRecords = {
             
             CONSTRUCT { ?s ?p ?o .   } WHERE { 
                 {
-                    ?s rdf:type schema:ImageObject .
-                    BIND(rdf:type AS ?p)
-                    BIND(schema:ImageObject as ?o)
+                    ?s ?p ?o .
+                    FILTER EXISTS { ?s a schema:ImageGallery}
                 }
                 UNION
                 {
-                    ?s rdf:type dcat:Distribution .
-                    BIND(rdf:type AS ?p)
-                    BIND(dcat:Distribution as ?o)
+                    ?s ?p ?o .
+                    FILTER EXISTS { ?s a dcat:Catalog}
                 }
-            
             }
             `
+
     }
 }
